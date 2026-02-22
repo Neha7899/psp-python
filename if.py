51610eb50dc25101ec4d9ea6246e53fedd0e100d
neha7899 <anehareddy7@gmail.com>
2026-02-22 19:31:31 +0530

    if conditional or decisional statement

---
+++ if.py
@@ -0,0 +1,20 @@
+#WAP to check whether the given integer is even or not
+# a=int(input("enter the number:"))
+# if a%2==0:
+#     print("the given number is even")
+
+#WAP to check whether the given number is odd
+# n=int(input("Enter the number:"))
+# if n%2!=0:         #or if n%2==1:
+#     print("the given number is odd")
+
+#WAP to check whether given string has exactly 5 characters in it 
+# s=input("enter the string:")
+# if len(s)==5:
+#     print("the given string has exactly 5 characters")
+
+#WAP to check whether the given integer is greater than 200
+# n=int(input("enter the number:"))
+# if n>200:
+#     print("the given number is greater than 200")
+
