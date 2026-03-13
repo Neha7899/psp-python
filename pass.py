4d98d741ce01d56b697b5cb6f1948cdfafb1bb49
neha7899 <anehareddy7@gmail.com>
2026-03-13 16:00:09 +0530

    pass intermediate termination

---
+++ pass.py
@@ -0,0 +1,6 @@
+# for i in range(1,10):
+#     if i==3:
+#         pass#placeholder #leaving an empty block to fill it with break,continue or any other syntax in future at the place of pass keyword
+#     print(i)
+
+
