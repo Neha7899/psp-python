41cc5b27a04c052d2c3e6ee959931712e9464977
neha7899 <anehareddy7@gmail.com>
2026-03-13 16:08:27 +0530

    unpacking

---
+++ unpacking.py
@@ -0,0 +1,32 @@
+#unpacking
+
+#sample programs
+
+# def unpack(v1,v2,v3,v4):
+#     print(v1,v2,v3,v4)
+# unpack(*'abcd')
+#op=a b c d
+
+
+# def unpack(v1,v2,v3,v4):
+#     print(v1,v2,v3,v4)
+# unpack(*[10,20,30,40])
+#op=10 20 30 40
+
+# def unpack(v1,v2,v3,v4):
+#     print(v1,v2,v3,v4)
+# unpack(*{'a':10,'b':20,'c':30,'d':40})
+#op=a b c d
+
+# def unpack(v1,v2,v3,v4):
+#     print(v1,v2,v3,v4)
+# unpack(*{'a':10,'b':20,'c':30,'d':40}.items())
+#op=('a', 10) ('b', 20) ('c', 30) ('d', 40)
+
+
+# def unpack(v1,v2,v3,v4):
+#     print(v1,v2,v3,v4)
+# unpack(*(range(1,5)))
+#op=1 2 3 4
+
+
