137070492bb9d6a7ddd02e92a1f37d8506fbac3f
neha7899 <anehareddy7@gmail.com>
2026-03-13 16:14:39 +0530

    in-built functions in string datatype

--- string_datatype.py
+++ string_datatype.py
@@ -1,2 +1,17 @@
-a='john'
-print(a)
+# z='john'
+# print(z)
+
+
+ #functions on strings
+# a="Python is easy"
+# print(a.upper())
+# print(a.lower())
+# print(a.swapcase())
+# print(a.replace('easy','difficult'))
+# print(a.replace('i','*'))
+# print(a.capitalize())
+# print(a.title())#capitalizes all the first letter in each word of the sentence given
+# print(a.index('e'))
+# print(a.count('s'))
+# print(a.split('s'))#splits upto the given character
+# print(a.split())
