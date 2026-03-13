56a5c057988c496223a19293f4d1ca6a49f48a24
neha7899 <anehareddy7@gmail.com>
2026-03-13 16:02:53 +0530

    in-built functions of tuple datatype

---
+++ in_built_functions_of_tuple.py
@@ -0,0 +1,17 @@
+a=(10,20,30,40,50,60)
+print(a)
+print(id(a))
+print(type(a))
+print(len(a))
+
+
+#index()
+x=a.index(10)
+print(x)
+
+x=a.index(20,1,3)
+print(x)
+
+#count()
+y=a.count(20)
+print(y)
