c4dc483b6e97a99747b655c19014a05c0c30c9e3
neha7899 <anehareddy7@gmail.com>
2026-03-13 15:59:08 +0530

    continue intermediate termination

---
+++ continue.py
@@ -0,0 +1,10 @@
+# for i in range(1,10):
+#     if i==4:
+#         continue#continuing the loop at 4,means skipping 4 and continuing the next
+#     print(i)
+
+
+# for i in range(1,21):
+#     if i%2==0:#to get even numbers
+#         continue#skips all the even numbers and prints only odd numbers
+#     print(i)
