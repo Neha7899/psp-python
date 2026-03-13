206d0fd61ff78fbf72949fe5ebbabd011a4dacf8
neha7899 <anehareddy7@gmail.com>
2026-03-13 16:11:27 +0530

    recursion and two types of recursion 1.with return value 2.without return value

---
+++ recursion.py
@@ -0,0 +1,66 @@
+#1.with return value
+
+# WAP to find the factorial of a given number
+# def fact(n):
+#     if n==1 or n==0:
+#         return 1
+#     return n*fact(n-1)
+# print(fact(int(input('enter a number:'))))    #or print(fact(3))
+
+
+# WAP to find the sum of n natural numbers(3->3+2+1)
+# def sum(n):
+#     if n==1:
+#         return 1
+#     return n+sum(n-1)
+# print(sum(5))
+
+
+
+#2.without return value
+
+# WAP to print the series as follows #5 4 3 2 1 2 3 4 5 #sam()->series as follows
+# def sam(n):
+#     print(n,end='  ')
+#     if n==1:
+#         return
+#     sam(n-1)
+#     print(n,end='  ')
+# sam(5)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
